3bd5c64267df2001ceedeb27721562a4bb1005a2
Rosa <rosa.morales.rojas@gmail.com>
2018-09-04 23:35:51 -0300

    Se eliminan ciertos estilos innecesarios

--- app.js
+++ app.js
@@ -1,40 +1,25 @@
 $(document).ready(function(){
-	// alert('holaaaa');
 
 	var web = 'https://mindicador.cl/api';
-	var uf, utm, euro, dolar;
-
+	var dolar, uf, utm, euro;
+	
 	$.ajax({
 		url: web, 
 		type: 'GET'
 	}).then(function(data){
 		console.log(data);
+		dolar = data.dolar.valor;
 		uf = data.uf.valor;
 		utm = data.utm.valor;
 		euro = data.euro.valor;
-		dolar = data.dolar.valor;	
 		peso = 1 / dolar;
 
-// console.log(euro)
+		// console.log(euro)
+
+		$('#dolar').text('$' + dolar.toLocaleString('es-CL'));
+		$('#euro').text('$' + euro.toLocaleString('es-CL'));
 		$('#uf').text('$' + uf.toLocaleString('es-CL'));
 		$('#utm').text('$' + utm.toLocaleString('es-CL'));
-		$('#euro').text('$' + euro.toLocaleString('es-CL'));
-		$('#dolar').text('$' + dolar.toLocaleString('es-CL'));
 
-	$('#conversor').click(function(){
-		// alert(dolar);
-		 var pesos = $('#pesos').val(); 
-		 $('#resultado-dolar').text(pesos / dolar);
-		 $('#resultado-euro').text(pesos / euro);
-
-});
-
-	$('#conversor--dolar').click(function(){
-		// alert	
-		var dolar = $('#dolar--americano').val();
-		$('#resultado-peso').text(  dolar / peso );
-		console.log(dolar);
 	});
 });
-
-
